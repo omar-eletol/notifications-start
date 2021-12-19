295d882106d64982e47c66c5c4548b5ed40f7dc9
omar eletol <omareletol@gmail.com>
2021-12-20 01:23:50 +0200

    clear badges

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -69,7 +69,11 @@ class EggTimerFragment : Fragment() {
                 NotificationManager.IMPORTANCE_HIGH
             )
 
-            // TODO: Step 2.6 disable badges for this channel
+                // TODO: Step 2.6 disable badges for this channel
+                .apply {
+                    setShowBadge(false)
+                }
+
             notificationChannel.enableLights(true)
             notificationChannel.lightColor = Color.RED
             notificationChannel.enableVibration(true)
