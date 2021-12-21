4bbd676ba347e286896fbee06eb005cc0593d905
omar eletol <omareletol@gmail.com>
2021-12-21 05:49:16 +0200

    handle data messages and foreground,background notifications

--- app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
@@ -1,15 +1,37 @@
 package com.example.android.eggtimernotifications
 
+import android.app.NotificationManager
 import android.content.ContentValues.TAG
 import android.nfc.Tag
 import android.util.Log
+import androidx.core.content.ContextCompat
+import com.example.android.eggtimernotifications.util.sendNotification
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
+    // [START receive_message]
     override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: ${remoteMessage?.from}")
+
+        // TODO: Step 3.5 check messages for data
+        // Check if message contains a data payload.
+        remoteMessage?.data?.let {
+            Log.d(TAG, "Message data payload: " + remoteMessage.data)
+        }
+
+        // TODO: Step 3.6 check messages for notification and call sendNotification
+        // Check if message contains a notification payload.
+        remoteMessage?.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+            sendNotification(it.body!!)
+        }
+
     }
+
+
+    // [END receive_message]
 
     // TODO: Step 3.2 log registration token
     // [START on_new_token]
@@ -30,6 +52,19 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     private fun sendRegistrationToServer(token: String?) {
         TODO("Not yet implemented")
+    }
+
+    private fun sendNotification(messageBody: String) {
+        val notificationManager = ContextCompat.getSystemService(
+            applicationContext,
+            NotificationManager::class.java
+        ) as NotificationManager
+        notificationManager.sendNotification(messageBody, applicationContext)
+
+    }
+
+    companion object {
+        private const val TAG = "MyFirebaseMesService"
     }
 
 }
